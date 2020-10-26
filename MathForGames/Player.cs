3a832ace2867d787c9850c0bb29a38c37689dd33
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-10-26 09:20:18 -0500

    fixed most

--- MathForGames/Player.cs
+++ MathForGames/Player.cs
@@ -23,63 +23,64 @@ namespace MathForGames
             }
         }
 
+        /// <param name="x">Position on the x axis</param>
+        /// <param name="y">Position on the y axis</param>
+        /// <param name="icon">The symbol that will appear when drawn</param>
+        /// <param name="color">The color of the symbol that will appear when drawn</param>
         public Player(float x, float y, char icon = ' ', ConsoleColor color = ConsoleColor.White)
             : base(x, y, icon, color)
         {
 
-            
         }
 
+        /// <param name="x">Position on the x axis</param>
+        /// <param name="y">Position on the y axis</param>
+        /// <param name="rayColor">The color of the symbol that will appear when drawn to raylib</param>
+        /// <param name="icon">The symbol that will appear when drawn</param>
+        /// <param name="color">The color of the symbol that will appear when drawn to the console</param>
         public Player(float x, float y, Color rayColor, char icon = ' ', ConsoleColor color = ConsoleColor.White)
-            : this(x, y, icon, color)
+            : base(x, y, rayColor, icon, color)
         {
-            _rayColor = rayColor;
 
         }
 
         public override void Update(float deltaTime)
         {
 
-            int xVelocity = -Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_A)) +
+            int xDirection = -Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_A)) +
                 Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_D));
 
-            int xVelocity = -Convert.ToInt32(Game.GetKeyDown((int)(KeyboardKey.KEY_A))
-                + Convert.ToInt32(Game.GetKeyDown((int)(KeyboardKey.KEY_D));
-
-
-            int yVelocity = -Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_W)) +
+            int yDirection = -Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_W)) +
                 Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_S));
 
-            Velocity = new Vector2(xVelocity, yVelocity);
+            Velocity = new Vector2(xDirection, yDirection);
             Velocity = Velocity.Normalized * Speed;
 
             base.Update(deltaTime);
         }
 
 
-            ConsoleKey keyPressed = Game.GetNextKey();
+            //ConsoleKey keyPressed = Game.GetNextKey();
 
-            switch (keyPressed)
-            {
-                case ConsoleKey.A:
-                    _velocity.X = -2;
-                    break;
-                case ConsoleKey.D:
-                    _velocity.X = 2;
-                    break;
-                case ConsoleKey.W:
-                    _velocity.Y = -2;
-                    break;
-                case ConsoleKey.S:
-                    _velocity.Y = 2;
-                    break;
-                default:
-                    _velocity.X = 0;
-                    _velocity.Y = 0;
-                    break;
-            }
-            base.Update();
-        }
+            //switch (keyPressed)
+            //{
+            //    case ConsoleKey.A:
+            //        _velocity.X = -2;
+            //        break;
+            //    case ConsoleKey.D:
+            //        _velocity.X = 2;
+            //        break;
+            //    case ConsoleKey.W:
+            //        _velocity.Y = -2;
+            //        break;
+            //    case ConsoleKey.S:
+            //        _velocity.Y = 2;
+            //        break;
+            //    default:
+            //        _velocity.X = 0;
+            //        _velocity.Y = 0;
+            //        break;
+            //}
 
     }
 }
